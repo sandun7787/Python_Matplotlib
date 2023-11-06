d89b781596d7932071380801f5b977801a07cb08
sandun7787 <sandunwarnasooreya345@gmail.com>
2023-11-06 09:57:02 +0530

    matplotlib new array

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ plt.xlabel('x axis')
 plt.ylabel('y axis')
 plt.title('show')
 plt.show()"""
-
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -26,3 +26,32 @@ print(bottom)
 print(top)
 plt.show()
 
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.arange(1,10,0.1)
+y1=x
+y2=np.sqrt(x)
+y3=np.power(x,2)
+y4=np.power(x,3)
+plt.figure()
+
+plt.subplot(2,2,1)
+plt.plot(x,y1,'ro')
+plt.title('y1=x')
+
+plt.subplot(2,2,2)
+plt.plot(x,y2,'g--')
+plt.title('y1=x')
+
+plt.subplot(2,2,3)
+plt.plot(x,y3,'b^')
+plt.title('$y3=x^2$')
+plt.show()
+
+plt.subplot(2,2,4)
+plt.plot(x,y4,'ks')
+plt.title('$y4=x^3$')
+plt.show()
